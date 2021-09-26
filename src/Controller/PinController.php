3d5e8bc7ec06c9f7c6f55618cd806f2bb4bc9cbb
juldblack <juliendunia@hotmail.com>
2021-09-26 20:19:22 +0200

    creation pin controller, entité pin, BD

---
+++ src/Controller/PinController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\Pin;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PinController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(): Response
+    {
+        $pin = new Pin;
+         
+         
+
+        return $this->render('pin/index.html.twig', [
+            'controller_name' => 'PinController',
+        ]);
+        //return new Response("Hello World");
+        //return $this->json(['message' => 'Hello World']);
+    }
+}
